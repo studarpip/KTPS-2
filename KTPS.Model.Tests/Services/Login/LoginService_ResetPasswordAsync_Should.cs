01ce219db368d141ee1f20e214e1d6b6277314e1
Gvidas28 <gvidasluk28@gmail.com>
2024-04-07 23:21:53 +0300

    (APP-51) Test for ResetPasswordAsync: ReturnErrorIfUserDoesNotExist

---
+++ KTPS.Model.Tests/Services/Login/LoginService_ResetPasswordAsync_Should.cs
@@ -0,0 +1,27 @@
+﻿using KTPS.Model.Entities;
+using KTPS.Model.Entities.Requests;
+using KTPS.Model.Entities.User;
+using KTPS.Model.Repositories.PasswordReset;
+using KTPS.Model.Services.Login;
+using KTPS.Model.Services.User;
+
+namespace KTPS.Model.Tests.Services.Login;
+
+public class LoginService_ResetPasswordAsync_Should
+{
+    [Fact]
+    public async void ReturnErrorIfUserDoesNotExist()
+    {
+        var userService = new Mock<IUserService>();
+        var passwordResetRepository = new Mock<IPasswordResetRepository>();
+
+        var loginService = new LoginService(userService.Object, passwordResetRepository.Object);
+        userService.Setup(_ => _.GetUserByIdAsync(5)).ReturnsAsync(null as UserBasic);
+
+        var resetPaswordRequest = new ResetPasswordRequest() { UserID = 5 };
+        var res = await loginService.ResetPasswordAsync(resetPaswordRequest);
+
+        var exceptedRes = new ServerResult() { Success = false, Message = "User not found!" };
+        res.Should().BeEquivalentTo(exceptedRes);
+    }
+}
