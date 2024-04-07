9b309e62955962a220e6eb69d741d826c9c83f79
pamesta <pamestapinigine@gmail.com>
2024-04-07 21:00:59 +0300

    APP-49 registration auth return error if auth code do not match failing test

--- KTPS.Model.Tests/Services/Registration/RegistrationService_AuthRegistration_Should.cs
+++ KTPS.Model.Tests/Services/Registration/RegistrationService_AuthRegistration_Should.cs
@@ -14,9 +14,6 @@ public class RegistrationService_AuthRegistration_Should
     [Fact]
     public async void ReturnErrorIfRegistrationDoesNotExist()
     {
-        var someUsername = "someUsername";
-        var someEmail = "someEmail";
-        var somePassword = "somePassword";
         var someId = 1;
 
         var userService = new Mock<IUserService>();
@@ -28,6 +25,28 @@ public class RegistrationService_AuthRegistration_Should
 
         var request = new RegistrationAuthRequest() { RegistrationID = someId, AuthCode = "not important" };
         var expected = new ServerResult<int>() { Success = false, Message = "Registration does not exist!" };
+
+        var result = await registrationService.AuthRegistrationAsync(request);
+
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public async void ReturnErrorIfAuthCodesDoNotMatch()
+    {
+        var someId = 1;
+        var someAuthCode = "someAuthCode";
+
+        var userService = new Mock<IUserService>();
+
+        var registrationRepo = new Mock<IRegistrationRepository>();
+        var someRegistrationBasic = new RegistrationBasic() { ID = someId, AuthCode = someAuthCode };
+        registrationRepo.Setup(_ => _.GetByID(someId)).ReturnsAsync(someRegistrationBasic);
+
+        var registrationService = new RegistrationService(userService.Object, registrationRepo.Object);
+
+        var request = new RegistrationAuthRequest() { RegistrationID = someId, AuthCode = "mismatchingCode" };
+        var expected = new ServerResult<int>() { Success = false, Message = "Authentication code is incorrect!" };
 
         var result = await registrationService.AuthRegistrationAsync(request);
 
