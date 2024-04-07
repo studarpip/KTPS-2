d4a4b8f1b8f4b09f599149c9fded639f61d8fe85
Gvidas28 <gvidasluk28@gmail.com>
2024-04-07 22:52:39 +0300

    (APP-51) Test for LoginAsync: ReturnErrorIfUserDoesNotExist

---
+++ KTPS.Model.Tests/Services/Login/LoginService_LoginAsync_Should.cs
@@ -0,0 +1,27 @@
+﻿using KTPS.Model.Entities;
+using KTPS.Model.Entities.Requests;
+using KTPS.Model.Entities.User;
+using KTPS.Model.Repositories.PasswordReset;
+using KTPS.Model.Services.Login;
+using KTPS.Model.Services.User;
+
+namespace KTPS.Model.Tests.Services.Login;
+
+public class LoginService_LoginAsync_Should
+{
+    [Fact]
+    public async void ReturnErrorIfUserDoesNotExist()
+    {
+        var userService = new Mock<UserService>();
+        var passwordResetRepository = new Mock<PasswordResetRepository>();
+
+        var loginService = new LoginService(userService.Object, passwordResetRepository.Object);
+        userService.Setup(_ => _.GetUserByUsernameAsync("test_user")).ReturnsAsync(null as UserBasic);
+
+        var loginRequest = new LoginRequest() { Username = "test_user", Password = "test_password" };
+        var res = await loginService.LoginAsync(loginRequest);
+
+        var exceptedRes = new ServerResult<int>() { Success = false, Message = "User does not exist!" };
+        res.Should().BeEquivalentTo(exceptedRes);
+    }
+}
--- KTPS.Model/Services/Login/LoginService.cs
+++ KTPS.Model/Services/Login/LoginService.cs
@@ -1,5 +1,7 @@
 ﻿using KTPS.Model.Entities;
 using KTPS.Model.Entities.Requests;
+using KTPS.Model.Repositories.PasswordReset;
+using KTPS.Model.Services.User;
 using System;
 using System.Threading.Tasks;
 
@@ -7,6 +9,18 @@ namespace KTPS.Model.Services.Login;
 
 public class LoginService : ILoginService
 {
+    private readonly IUserService _userService;
+    private readonly IPasswordResetRepository _passwordResetRepository;
+
+    public LoginService(
+        IUserService userService,
+        IPasswordResetRepository passwordResetRepository
+        )
+    {
+        _userService = userService;
+        _passwordResetRepository = passwordResetRepository;
+    }
+
     public async Task<ServerResult> ResetPasswordAsync(ResetPasswordRequest request) => throw new NotImplementedException();
 
     public async Task<ServerResult> ResetPasswordAuthAsync(ResetPasswordAuthRequest request) => throw new NotImplementedException();
