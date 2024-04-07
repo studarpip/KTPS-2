891e24f28b093f9ad698fb5276b8980c7a7598e6
pamesta <pamestapinigine@gmail.com>
2024-04-07 19:59:24 +0300

    APP-49 Create registration failing test

--- KTPS.Model.Tests/Services/Registration/RegistrationService_StartRegistration_Should.cs
+++ KTPS.Model.Tests/Services/Registration/RegistrationService_StartRegistration_Should.cs
@@ -1,5 +1,8 @@
 using KTPS.Model.Entities;
+using KTPS.Model.Entities.Registration;
 using KTPS.Model.Entities.Requests;
+using KTPS.Model.Helpers;
+using KTPS.Model.Repositories.Registration;
 using KTPS.Model.Services.Registration;
 using KTPS.Model.Services.User;
 
@@ -42,6 +45,39 @@ public class RegistrationService_StartRegistration_Should
 
         var request = new RegistrationStartRequest() { Username = someUsername, Email = someEmail, Password = somePassword };
         var expected = new ServerResult<int>() { Success = false, Message = "Email already exists!" };
+
+        var result = await registrationService.StartRegistrationAsync(request);
+
+        Assert.Equivalent(expected, result);
+    }
+
+    [Fact]
+    public async void CreateRegistrationBasicAndReturnRegistrationId()
+    {
+        var someUsername = "someUsername";
+        var someEmail = "someEmail";
+        var somePassword = "somePassword";
+        var someRegistrationId = 0;
+
+        var userService = new Mock<IUserService>();
+        userService.Setup(_ => _.EmailExistsAsync(someEmail)).ReturnsAsync(false);
+        userService.Setup(_ => _.UsernameExistsAsync(someUsername)).ReturnsAsync(false);
+
+        var registrationRepo = new Mock<IRegistrationRepository>();
+        var expectedRegistrationBasic = new RegistrationBasic()
+        {
+            UserID = null,
+            Username = someUsername,
+            Email = someEmail,
+            Password = somePassword.Hash(),
+            AuthCode = "asdfsadf"
+        };
+        registrationRepo.Setup(_ => _.InsertAsync(expectedRegistrationBasic)).ReturnsAsync(someRegistrationId);
+
+        var registrationService = new RegistrationService(userService.Object);
+
+        var request = new RegistrationStartRequest() { Username = someUsername, Email = someEmail, Password = somePassword };
+        var expected = new ServerResult<int>() { Success = true, Data = someRegistrationId };
 
         var result = await registrationService.StartRegistrationAsync(request);
 
