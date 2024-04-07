a9f227427672de0d14982bc2ca3c9d208c512f1b
arnasp <pipinisarnas@gmailcom>
2024-04-07 22:18:25 +0300

    APP-50 CallFriendRepositoryAndFindUsers failing test

---
+++ KTPS.Model.Tests/Services/Friends/FriendsService_FindFriend_Should.cs
@@ -0,0 +1,45 @@
+﻿using KTPS.Model.Entities;
+using KTPS.Model.Entities.Requests;
+using KTPS.Model.Entities.User;
+using KTPS.Model.Repositories.Friends;
+using KTPS.Model.Services.Friends;
+
+namespace KTPS.Model.Tests.Services.Friends
+{
+    public class FriendsService_FindFriend_Should
+    {
+        public class FriendsService_DeleteFriend_Should
+        {
+            [Fact]
+            public async void CallFriendRepositoryAndFindUsers()
+            {
+                var someUserId = 1;
+                var someInput = "input";
+
+                IEnumerable<UserMinimal> friendsList = new List<UserMinimal>()
+                {
+                    new UserMinimal() { ID = 1, Username = "input1" },
+                    new UserMinimal() { ID = 2, Username = "input2" },
+                };
+
+                var friendsRepository = new Mock<IFriendsRepository>();
+
+                friendsRepository.Setup(_ => _.FindFriendAsync(someInput)).ReturnsAsync(friendsList);
+
+                var friendsService = new FriendsService(friendsRepository.Object);
+
+                FindFriendRequest findFriendRequest = new FindFriendRequest
+                {
+                    UserID = someUserId,
+                    Input = someInput
+                };
+
+                var result = await friendsService.FindFriendAsync(findFriendRequest);
+
+                var expectedResult = new ServerResult<IEnumerable<UserMinimal>>() { Success = true, Data = friendsList.Where(x => x.ID != someUserId) };
+
+                result.Should().BeEquivalentTo(expectedResult);
+            }
+        }
+    }
+}
