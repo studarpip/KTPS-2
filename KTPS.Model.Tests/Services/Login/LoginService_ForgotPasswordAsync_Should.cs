9d32e8da5080ea455960ce09d0bf31cebbcfbc54
Gvidas28 <gvidasluk28@gmail.com>
2024-04-07 23:59:04 +0300

    (APP-51) Test for ForgotPassowrdAsync: ReturnUserAndGenerateCodeOnSuccess

--- KTPS.Model.Tests/Services/Login/LoginService_ForgotPasswordAsync_Should.cs
+++ KTPS.Model.Tests/Services/Login/LoginService_ForgotPasswordAsync_Should.cs
@@ -4,6 +4,7 @@ using KTPS.Model.Entities;
 using KTPS.Model.Repositories.PasswordReset;
 using KTPS.Model.Services.Login;
 using KTPS.Model.Services.User;
+using KTPS.Model.Helpers;
 
 namespace KTPS.Model.Tests.Services.Login
 {
@@ -24,6 +25,26 @@ namespace KTPS.Model.Tests.Services.Login
 
             var exceptedRes = new ServerResult<int>() { Success = false, Message = "User with this email does not exist!" };
             res.Should().BeEquivalentTo(exceptedRes);
+        }
+
+        [Fact]
+        public async void ReturnUserAndGenerateCodeOnSuccess()
+        {
+            var userService = new Mock<IUserService>();
+            var passwordResetRepository = new Mock<IPasswordResetRepository>();
+
+            var loginService = new LoginService(userService.Object, passwordResetRepository.Object);
+            userService.Setup(_ => _.GetUserByEmailAsync("some_email")).ReturnsAsync(new UserBasic() { ID = 1, Email = "some_email", Username = "name", Password = "password" });
+
+            var forgotPassswordRequest = new ForgotPasswordRequest() { Email = "some_email" };
+
+            var res = await loginService.ForgotPasswordAsync(forgotPassswordRequest);
+
+            var authCode = RandomString.GenerateRandomString();
+
+            var exceptedRes = new ServerResult<int>() { Success = true, Data = 1 };
+            res.Should().BeEquivalentTo(exceptedRes);
+            authCode.Count().Should().BeOneOf(6);
         }
     }
 }
