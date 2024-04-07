92b70037c0d646b068fdc5a394dc3aa6ea4c9485
Gvidas28 <gvidasluk28@gmail.com>
2024-04-07 23:48:26 +0300

    (APP-51) Test for ResetPasswordAsync: ReturnSuccessfulResultAndChangePassword

--- KTPS.Model.Tests/Services/Login/LoginService_ResetPasswordAsync_Should.cs
+++ KTPS.Model.Tests/Services/Login/LoginService_ResetPasswordAsync_Should.cs
@@ -1,7 +1,9 @@
 ﻿using KTPS.Model.Entities;
 using KTPS.Model.Entities.Requests;
 using KTPS.Model.Entities.User;
+using KTPS.Model.Helpers;
 using KTPS.Model.Repositories.PasswordReset;
+using KTPS.Model.Repositories.User;
 using KTPS.Model.Services.Login;
 using KTPS.Model.Services.User;
 
@@ -40,5 +42,30 @@ public class LoginService_ResetPasswordAsync_Should
 
         var exceptedRes = new ServerResult() { Success = false, Message = "Technical error!" };
         res.Should().BeEquivalentTo(exceptedRes);
+    }
+
+    [Fact]
+    public async void ReturnSuccessfulResultAndChangePassword()
+    {
+        var passwordResetRepository = new Mock<IPasswordResetRepository>();
+        var userRepository = new Mock<IUserRepository>();
+        var userServiceMock = new Mock<IUserService>();
+        var userService = new UserService(userRepository.Object);
+
+        var loginService = new LoginService(userServiceMock.Object, passwordResetRepository.Object);
+        userServiceMock.Setup(_ => _.GetUserByIdAsync(5)).ReturnsAsync(new UserBasic());
+        passwordResetRepository.Setup(_ => _.GetCodeAsync(5)).ReturnsAsync("abcdef");
+
+        var resetPaswordRequest = new ResetPasswordRequest() { UserID = 5, AuthCheck = "abcdef", NewPassword = "test_password" };
+        var res = await loginService.ResetPasswordAsync(resetPaswordRequest);
+
+        var expecetedRes = new ServerResult { Success = true };
+        res.Should().BeEquivalentTo(expecetedRes);
+
+        userRepository.Setup(_ => _.GetByIdAsync(5)).ReturnsAsync(new UserBasic { Password = resetPaswordRequest.NewPassword.Hash() });
+        var updatedUser = await userService.GetUserByIdAsync(5);
+
+        var expectedRes2 = new UserBasic { Password = resetPaswordRequest.NewPassword.Hash() };
+        updatedUser.Should().BeEquivalentTo(expectedRes2);
     }
 }
