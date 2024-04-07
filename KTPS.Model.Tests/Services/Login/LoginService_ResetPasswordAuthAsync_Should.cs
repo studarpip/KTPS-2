2d384de37c7683ba405c1caba2f31e8b47bd2bb7
Gvidas28 <gvidasluk28@gmail.com>
2024-04-08 00:03:45 +0300

    (APP-51) Test for ResetPasswordAuthAsync: ReturnErrorIfRecoveryCodeNotCorrect

---
+++ KTPS.Model.Tests/Services/Login/LoginService_ResetPasswordAuthAsync_Should.cs
@@ -0,0 +1,27 @@
+﻿using KTPS.Model.Entities;
+using KTPS.Model.Entities.Requests;
+using KTPS.Model.Repositories.PasswordReset;
+using KTPS.Model.Services.Login;
+using KTPS.Model.Services.User;
+
+namespace KTPS.Model.Tests.Services.Login
+{
+    public class LoginServiceResetPasswordAuthAsync_Should
+    {
+        [Fact]
+        public async void ReturnErrorIfRecoveryCodeNotCorrect()
+        {
+            var userService = new Mock<IUserService>();
+            var passwordResetRepository = new Mock<IPasswordResetRepository>();
+
+            var loginService = new LoginService(userService.Object, passwordResetRepository.Object);
+            passwordResetRepository.Setup(_ => _.GetCodeAsync(1)).ReturnsAsync("code");
+
+            var resetPasswordAuthRequest = new ResetPasswordAuthRequest() { UserID = 1, RecoveryCode = "wrong_code" };
+            var res = await loginService.ResetPasswordAuthAsync(resetPasswordAuthRequest);
+
+            var exceptedRes = new ServerResult() { Success = false, Message = "Recovery code incorrect!" };
+            res.Should().BeEquivalentTo(exceptedRes);
+        }
+    }
+}
