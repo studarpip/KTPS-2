3cc352920c95b8dd91ac6f50f7bc7042657d1506
Gvidas28 <gvidasluk28@gmail.com>
2024-04-07 22:34:45 +0300

    (APP-51) Create the LoginService with empty methods for TDD

---
+++ KTPS.Model/Services/Login/LoginService.cs
@@ -0,0 +1,17 @@
+﻿using KTPS.Model.Entities;
+using KTPS.Model.Entities.Requests;
+using System;
+using System.Threading.Tasks;
+
+namespace KTPS.Model.Services.Login;
+
+public class LoginService : ILoginService
+{
+    public async Task<ServerResult> ResetPasswordAsync(ResetPasswordRequest request) => throw new NotImplementedException();
+
+    public async Task<ServerResult> ResetPasswordAuthAsync(ResetPasswordAuthRequest request) => throw new NotImplementedException();
+
+    public async Task<ServerResult<int>> ForgotPasswordAsync(ForgotPasswordRequest request) => throw new NotImplementedException();
+
+    public async Task<ServerResult<int>> LoginAsync(LoginRequest request) => throw new NotImplementedException();
+}
