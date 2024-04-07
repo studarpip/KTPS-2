3bfab1f2b03c6cfd91cb1a0e9600553c79dff1c7
Gvidas28 <gvidasluk28@gmail.com>
2024-04-08 00:15:23 +0300

    (APP-14) Creating LoginController

---
+++ KTPS.Server.WebAPI/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using KTPS.Model.Entities;
+using KTPS.Model.Entities.Requests;
+using KTPS.Model.Services.Login;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace KTPS.Server.WebAPI.Controllers;
+
+[Controller, Route("/login")]
+public class LoginController
+{
+    private readonly ILoginService _loginService;
+
+    public LoginController(ILoginService loginService)
+    {
+        _loginService = loginService;
+    }
+
+    [HttpPost("login")]
+    public async Task<ServerResult<int>> Login([FromBody] LoginRequest request) => await _loginService.LoginAsync(request);
+
+    [HttpPost("forgotMyPassword")]
+    public async Task<ServerResult<int>> ForgotMyPassword([FromBody] ForgotPasswordRequest request) => await _loginService.ForgotPasswordAsync(request);
+
+    [HttpPost("resetAuth")]
+    public async Task<ServerResult> ResetAuth([FromBody] ResetPasswordAuthRequest request) => await _loginService.ResetPasswordAuthAsync(request);
+
+    [HttpPost("reset")]
+    public async Task<ServerResult> Reset([FromBody] ResetPasswordRequest request) => await _loginService.ResetPasswordAsync(request);
+
+}
