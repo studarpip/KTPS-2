c6b8f06505cea58b6b7a81b1d614679838d5ef75
pamesta <pamestapinigine@gmail.com>
2024-04-08 21:51:12 +0300

    integrate E2E framework

---
+++ KTPS.Server.WebAPI.E2E/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace KTPS.Server.WebAPI.E2E;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var tests = new List<E2E>
+        {
+            new RegistrationFlowE2E()
+        };
+
+        foreach (var test in tests)
+        {
+            await test.Test();
+        }
+    }
+}
+
+
+interface E2E
+{
+    Task Test();
+}
+
