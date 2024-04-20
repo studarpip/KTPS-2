e94d5b09147a0e341c4663bb2635f6cba5af1de6
arnasp <pipinisarnas@gmailcom>
2024-04-20 20:16:20 +0300

    add controllers

---
+++ KTPS.Server.WebAPI/Controllers/GroupsController.cs
@@ -0,0 +1,44 @@
+﻿using KTPS.Model.Entities;
+using KTPS.Model.Entities.Groups;
+using KTPS.Model.Entities.Requests;
+using KTPS.Model.Entities.Responses;
+using KTPS.Model.Services.Groups;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KTPS.Server.WebAPI.Controllers;
+
+[Controller, Route("groups")]
+public class GroupsController
+{
+    private readonly IGroupsService _groupsService;
+
+    public GroupsController(
+        IGroupsService groupsService
+        )
+    {
+        _groupsService = groupsService;
+    }
+
+    [HttpPost("new")]
+    public async Task<ServerResult<int>> NewAsync([FromBody] NewGroupRequest request) => await _groupsService.NewGroupAsync(request);
+
+    [HttpPost("edit")]
+    public async Task<ServerResult> EditAsync([FromBody] EditGroupRequest request) => await _groupsService.EditGroupAsync(request);
+
+    [HttpPost("delete")]
+    public async Task<ServerResult> DeleteAsync([FromBody] DeleteGroupRequest request) => await _groupsService.DeleteGroupAsync(request);
+
+    [HttpGet("users/{userId}/list")]
+    public async Task<ServerResult<IEnumerable<GroupBasic>>> ListAsync(int userId) => await _groupsService.GetGroupListAsync(userId);
+
+    [HttpPost("remove_members")]
+    public async Task<ServerResult> RemoveMembersAsync([FromBody] RemoveGroupMembersRequest request) => await _groupsService.RemoveGroupMembersAsync(request);
+
+    [HttpGet("{groupId}/members")]
+    public async Task<ServerResult<GetGroupMembersResponse>> MemberListAsync(int groupId) => await _groupsService.GetMemberListAsync(groupId);
+
+    [HttpPost("add_guest")]
+    public async Task<ServerResult<int>> AddGuestAsync([FromBody] AddGuestRequest request) => await _groupsService.AddGuestAsync(request);
+}
---
+++ KTPS.Server.WebAPI/Controllers/NotificationController.cs
@@ -0,0 +1,34 @@
+﻿using KTPS.Model.Entities;
+using KTPS.Model.Entities.Notifications;
+using KTPS.Model.Entities.Requests;
+using KTPS.Model.Services.Notifications;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KTPS.Server.WebAPI.Controllers;
+
+[Controller, Route("notification")]
+public class NotificationController
+{
+    private readonly INotificationService _notificationService;
+
+    public NotificationController(
+        INotificationService notificationService
+        )
+    {
+        _notificationService = notificationService;
+    }
+
+    [HttpPost("create")]
+    public async Task<ServerResult<int>> Create([FromBody] CreateNotificationRequest request) => await _notificationService.CreateAsync(request);
+
+    [HttpGet("list/{userId}")]
+    public async Task<ServerResult<IEnumerable<Notification>>> List(int userId) => await _notificationService.ListAsync(userId);
+
+    [HttpPost("respond")]
+    public async Task<ServerResult> Login([FromBody] RespondNotificationRequest request) => await _notificationService.RespondAsync(request);
+
+
+
+}
